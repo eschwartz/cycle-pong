3cb1a361d5fb5e73237e27c592a65a3f03b99fe5
Edan Schwartz <eschwartz@aerisweather.net>
2015-08-26 11:39:24 -0500

    Fix random velocity

--- src/geometry/util.js
+++ src/geometry/util.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 //http://math.stackexchange.com/questions/143932/calculate-point-given-x-y-angle-and-distance
 export var move = ({x, y}) => ({dx, dy}) => speed => {
   return {
@@ -10,6 +12,30 @@ export var addPoints = (pointA, pointB) => ({
   x: pointA.x + pointB.x,
   y: pointA.y + pointB.y
 });
+
+export function randomVelocity(speed) {
+  var randomDir;
+  var isTooVertical = dir => (
+    (dir > radians(0.4) && dir < radians(0.6)) ||
+    (dir > radians(1.4) && dir < radians(1.6))
+  );
+
+  do {
+    randomDir = _.random(0, Math.PI * 2);
+  } while (isTooVertical(randomDir));
+  return {
+    x: Math.cos(randomDir) * speed,
+    y: Math.sin(randomDir) * speed
+  }
+}
+
+export function radians(radians) {
+  return Math.PI * radians;
+}
+
+export function randomSign() {
+  return Math.random() >= 0.5 ? 1 : -1;
+}
 
 
 export var toRadians = degrees => degrees * (Math.PI / 180);
--- src/pong.js
+++ src/pong.js
@@ -6,7 +6,7 @@ import createArray from './util/create-array';
 import makePixiDriver from './driver/pixi-driver.js';
 import {Point} from './geometry/geometries';
 import $tick from './util/tick';
-import {move, addPoints} from './geometry/util';
+import {move, addPoints, randomVelocity} from './geometry/util';
 import {Scheduler} from 'rx-dom';
 
 var {Observable, Subject} = Rx;
@@ -37,8 +37,8 @@ var Actions = {
   ScorePlayerB: state => _.extend(state, { playerBPoints: state.playerBPoints + 1}),
   ResetBall: state => {
     _.extend(state.entities.ball, {
-      position: Point(_.random(100, state.gameWidth - 100), _.random(100, state.gameHeight - 100)),
-      velocity: Point(0.5, 0)//{x: _.random(-1, 1, true), y: _.random(-1, 1, true)}
+      position: Point(_.random(200, state.gameWidth - 200), _.random(200, state.gameHeight - 200)),
+      velocity: randomVelocity(5)
     });
     return state;
   }
@@ -51,10 +51,35 @@ function intent(DOM) {
   };
 }
 
+
+/**
+ * Returns a stream of events
+ * defined by a predicate.
+ * Each event will only emit once, while the predicate is satisfied.
+ *
+ * eg.
+ *
+ * event(Observable.from([1, -1, -2, -3, 4, 5])(x => x > 0)).
+ *  subscribe(x => console.log('event', x)
+ *
+ *  // event, 1
+ *  // event, 4
+ */
+function event($source) {
+  return isEvent => $source.
+    map(item => ({
+      val: item,
+      isEvent: isEvent(item)
+    })).
+    // Only return a single collision event
+    distinctUntilChanged(x => x.isEvent).
+    filter(x => x.val).
+    map(x => x.val);
+}
+
 function model(actions) {
   var operations = new Subject();
   var $state = operations.
-    do(Log('Operation')).
     scan(initialState, (state, operation) => operation(state)).
     // Share makes it so that this stream only runs once
     // for each subscription
@@ -62,58 +87,36 @@ function model(actions) {
 
 
   actions.$advanceEntities.
-    do(Log('advance')).
     subscribe(operations);
 
+  var collisionEvent = event($state);
+  var $goalACollision = collisionEvent(state => state.entities.ball.position.x <= 0);
+  var $goalBCollision = collisionEvent(state => state.entities.ball.position.x >= state.gameWidth);
+  var $wallTopCollision = collisionEvent(state => state.entities.ball.position.y <= 0);
+  var $wallBottomCollision = collisionEvent(state => state.entities.ball.position.y >= state.gameHeight);
 
-  function collision($state) {
-    return collisionTest => $state.
-      map(collisionTest).
-      // Only return a single collision event
-      distinctUntilChanged().
-      filter(isCollision => isCollision);
-  }
-
-  collision($state)(state => state.entities.ball.position.x >= state.gameWidth)
-
-  var $playerBGoalCollision = $state.
-    map(state => state.entities.ball.position.x >= state.gameWidth).
-    distinctUntilChanged().
-    filter(isCollision => isCollision);
-
-  var $playerAGoalCollision = $state.
-    map(state => state.entities.ball.position.x <= 0).
-    distinctUntilChanged().
-    filter(isCollision => isCollision);
-
-  $playerBGoalCollision.
-    map(() => Actions.ScorePlayerA).
-    subscribe(operations);
-
-  $playerAGoalCollision.
+  $goalACollision.
     map(() => Actions.ScorePlayerB).
     subscribe(operations);
 
+  $goalBCollision.
+    map(() => Actions.ScorePlayerA).
+    subscribe(operations);
 
+  $wallTopCollision.
+    map(() => Actions.BounceDown).
+    subscribe(operations);
 
+  $wallBottomCollision.
+    map(() => Actions.BounceUp).
+    subscribe(operations);
 
-  Observable.merge($playerAGoalCollision, $playerBGoalCollision).
+  Observable.merge($goalACollision, $goalBCollision).
     map(() => Actions.ResetBall).
     subscribe(operations);
 
-
-  /*var $wallTopCollisions = $state.
-    filter(state => state.entities.ball.position.y <= 0);
-  var $wallBottomCollisions = $state.
-    filter(state => state.entities.ball.position.y >= state.gameHeight);
-
-  $wallTopCollisions.
-    map(() => Actions.BounceDown).
-    subscribe(operations);
-  $wallBottomCollisions.
-    map(() => Actions.BounceUp).
-    subscribe(operations);*/
-
+  // Start out with the ball in a random location
+  operations.onNext(Actions.ResetBall);
 
   return $state;
 }
